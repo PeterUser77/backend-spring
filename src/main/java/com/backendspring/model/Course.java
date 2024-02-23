edfcd2625a64385a2ebd61e69a84360fc00895ec
Pedro Henrique Alves Almeida <pedrohenrique9592@gmail.com>
2024-02-23 08:40:55 -0300

    deleting lombok dependency

--- src/main/java/com/backendspring/model/Course.java
+++ src/main/java/com/backendspring/model/Course.java
@@ -20,16 +20,15 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import lombok.Data;
 
 @SQLDelete(sql = "UPDATE COURSE C SET C.STATUS = 'inactive' WHERE C.ID = ?")
 @Where(clause = "STATUS = 'active'")
-@Data
 @Entity
 @Table(name = "COURSE")
 public class Course {
@@ -43,6 +42,7 @@ public class Course {
 
     @NotNull
     @NotBlank
+    @NotEmpty
     @Length(
         min = 2,
         max = 50)
@@ -67,10 +67,72 @@ public class Course {
     @Convert(converter = StatusConverter.class)
     private Status status = Status.ACTIVE;
 
+    @NotNull
+    @NotEmpty
+    @Valid
     @OneToMany(
         cascade = CascadeType.ALL,
         orphanRemoval = true,
         mappedBy = "course")
     private List<Lesson> lessons = new ArrayList<>();
+
+    public Course() {
+    }
+
+    public Course(
+        Long id, 
+        @NotNull @NotBlank @Length(min = 2, max = 50) String name, 
+        @NotNull Category category,
+        @NotNull Status status, 
+        @NotBlank @NotNull @NotEmpty List<Lesson> lessons
+    ) {
+        this.id = id;
+        this.name = name;
+        this.category = category;
+        this.status = status;
+        this.lessons = lessons;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Category getCategory() {
+        return category;
+    }
+
+    public void setCategory(Category category) {
+        this.category = category;
+    }
+
+    public Status getStatus() {
+        return status;
+    }
+
+    public void setStatus(Status status) {
+        this.status = status;
+    }
+
+    public List<Lesson> getLessons() {
+        return lessons;
+    }
+
+    public void setLessons(List<Lesson> lessons) {
+        this.lessons = lessons;
+    }
+
+    
 
 }
