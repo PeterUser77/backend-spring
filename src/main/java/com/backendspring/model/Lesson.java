41da8089d093dc59a2129d789b429f622115caaa
Pedro Henrique Alves Almeida <pedrohenrique9592@gmail.com>
2024-02-23 08:36:39 -0300

    adding validate for lesson

--- src/main/java/com/backendspring/model/Lesson.java
+++ src/main/java/com/backendspring/model/Lesson.java
@@ -1,5 +1,7 @@
 package com.backendspring.model;
 
+import org.hibernate.validator.constraints.Length;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 
 import jakarta.persistence.Column;
@@ -10,10 +12,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
-import lombok.Data;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 
 
-@Data
 @Entity
 public class Lesson {
 
@@ -22,24 +24,137 @@ public class Lesson {
     @GeneratedValue(strategy = GenerationType.AUTO)
     private Long id;
 
+    @NotNull
+    @NotBlank
+    @Length(
+        min = 5, 
+        max = 50)
     @Column(
         name = "NAME",
-        length = 100,
+        length = 50,
         nullable = false
     )
     private String name;
 
+    @NotNull
+    @NotBlank
+    @Length(
+        min = 10, 
+        max = 11)
     @Column(
         name = "YOUTUBE_URL",
-        length = 100,
+        length = 11,
         nullable = false
     )
     private String youtubeUrl;
 
+    @NotNull
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
-    @JoinColumn(name = "COURSE_ID", nullable = false)
+    @JoinColumn(
+        name = "COURSE_ID", 
+        nullable = false)
     @JsonProperty(access = JsonProperty.Access.WRITE_ONLY)
     private Course course;
+
+    public Lesson() {
+    }
+
+    public Lesson(
+        Long id, 
+        @NotNull @NotBlank String name, 
+        @NotNull @NotBlank String youtubeUrl, 
+        @NotNull Course course
+    ) {
+        this.id = id;
+        this.name = name;
+        this.youtubeUrl = youtubeUrl;
+        this.course = course;
+    }
+
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getYoutubeUrl() {
+        return youtubeUrl;
+    }
+
+    public void setYoutubeUrl(String youtubeUrl) {
+        this.youtubeUrl = youtubeUrl;
+    }
+
+    public Course getCourse() {
+        return course;
+    }
+
+    public void setCourse(Course course) {
+        this.course = course;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        result = prime * result + ((name == null) ? 0 : name.hashCode());
+        result = prime * result + ((youtubeUrl == null) ? 0 : youtubeUrl.hashCode());
+        result = prime * result + ((course == null) ? 0 : course.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Lesson other = (Lesson) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        if (name == null) {
+            if (other.name != null)
+                return false;
+        } else if (!name.equals(other.name))
+            return false;
+        if (youtubeUrl == null) {
+            if (other.youtubeUrl != null)
+                return false;
+        } else if (!youtubeUrl.equals(other.youtubeUrl))
+            return false;
+        if (course == null) {
+            if (other.course != null)
+                return false;
+        } else if (!course.equals(other.course))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Lesson [id=" + id + ", name=" + name + ", youtubeUrl=" + youtubeUrl + ", course=" + course + "]";
+    }
+
+    
+    
 
 
 }
