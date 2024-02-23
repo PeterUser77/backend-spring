8ac649601faf0e1efd98dc70edd2e9f5f70a9505
Pedro Henrique Alves Almeida <pedrohenrique9592@gmail.com>
2024-02-23 08:37:53 -0300

    adding validate for category enum

--- src/main/java/com/backendspring/dto/CourseDTO.java
+++ src/main/java/com/backendspring/dto/CourseDTO.java
@@ -1,18 +1,21 @@
 package com.backendspring.dto;
 
 import java.util.List;
+import java.util.Locale.Category;
 
 import org.hibernate.validator.constraints.Length;
 
+import com.backendspring.enums.validation.ValueOfEnum;
 import com.fasterxml.jackson.annotation.JsonProperty;
 
+import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
 
 public record CourseDTO(
     
     @JsonProperty("_id") Long id,
-    @NotNull @NotBlank @Length(min = 2, max = 50) String name,
-    @NotNull @NotBlank @Pattern(regexp = "back-end|front-end") String category,
-    List<LessonDTO> lessons) { }
+    @NotNull @NotBlank @NotEmpty @Length(min = 2, max = 50) String name,
+    @NotNull @ValueOfEnum(enumClass = Category.class) String category,
+    @NotNull @NotEmpty @Valid  List<LessonDTO> lessons) { }
